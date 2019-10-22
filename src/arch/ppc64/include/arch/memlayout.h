0094dbd8fef58a7b973b0a09b6bdc8af33ce0adc
Marty E. Plummer <hanetzer@startmail.com>
2019-10-22 13:24:58 -0500

    util/crossgcc: build ppc64 targets with elfv2 abi
    Include TOC macro to place the ppc64 elfv2 abi Table of Contents
    
    Change-Id: I870f03ee9e97057d539b4764708a8245817c410a
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/arch/ppc64/include/arch/memlayout.h
+++ src/arch/ppc64/include/arch/memlayout.h
@@ -8,6 +8,16 @@
 /* TODO: Double-check that that's the correct alignment for our ABI. */
 #define STACK(addr, size) REGION(stack, addr, size, 8)
 
+#define TOC(addr, size) \
+	SYMBOL(toc, addr) \
+	.got : \
+	{ \
+		_toc_start = . + 0x8000; \
+		*(.got) \
+		*(.toc) \
+	} \
+	SYMBOL(etoc, addr + size)
+
 /* TODO: Need to add DMA_COHERENT region like on ARM? */
 
 #endif /* __ARCH_MEMLAYOUT_H */
