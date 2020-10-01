328fa98cc40953a6266ec00ba3bae8fdaddb8436
Marty E. Plummer <hanetzer@startmail.com>
2020-10-01 08:55:31 -0500

    lz4: fix endian issues?
    Change-Id: Ie12e69d1185cc53d448687c706d66bc7ae1f355d
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/commonlib/bsd/lz4_wrapper.c
+++ src/commonlib/bsd/lz4_wrapper.c
@@ -62,43 +62,7 @@ typedef uint64_t U64;
 #include "lz4.c.inc"	/* #include for inlining, do not link! */
 
 #define LZ4F_MAGICNUMBER 0x184D2204
-
-struct lz4_frame_header {
-	uint32_t magic;
-	union {
-		uint8_t flags;
-		struct {
-			uint8_t reserved0		: 2;
-			uint8_t has_content_checksum	: 1;
-			uint8_t has_content_size	: 1;
-			uint8_t has_block_checksum	: 1;
-			uint8_t independent_blocks	: 1;
-			uint8_t version			: 2;
-		};
-	};
-	union {
-		uint8_t block_descriptor;
-		struct {
-			uint8_t reserved1		: 4;
-			uint8_t max_block_size		: 3;
-			uint8_t reserved2		: 1;
-		};
-	};
-	/* + uint64_t content_size iff has_content_size is set */
-	/* + uint8_t header_checksum */
-} __packed;
-
-struct lz4_block_header {
-	union {
-		uint32_t raw;
-		struct {
-			uint32_t size		: 31;
-			uint32_t not_compressed	: 1;
-		};
-	};
-	/* + size bytes of data */
-	/* + uint32_t block_checksum iff has_block_checksum is set */
-} __packed;
+#define LZ4F_BLOCKUNCOMPRESSED_FLAG 0x80000000U
 
 size_t ulz4fn(const void *src, size_t srcn, void *dst, size_t dstn)
 {
@@ -108,53 +72,70 @@ size_t ulz4fn(const void *src, size_t srcn, void *dst, size_t dstn)
 	int has_block_checksum;
 
 	{ /* With in-place decompression the header may become invalid later. */
-		const struct lz4_frame_header *h = in;
+		uint32_t magic;
+		uint8_t flags, version, independent_blocks, has_content_size;
+		uint8_t block_desc;
 
-		if (srcn < sizeof(*h) + sizeof(uint64_t) + sizeof(uint8_t))
+		if (srcn < sizeof(uint32_t) + 3*sizeof(uint8_t))
 			return 0;	/* input overrun */
 
-		/* We assume there's always only a single, standard frame. */
-		if (le32toh(h->magic) != LZ4F_MAGICNUMBER || h->version != 1)
-			return 0;	/* unknown format */
-		if (h->reserved0 || h->reserved1 || h->reserved2)
-			return 0;	/* reserved must be zero */
-		if (!h->independent_blocks)
-			return 0;	/* we don't support block dependency */
-		has_block_checksum = h->has_block_checksum;
+		magic = le32toh(*(const uint32_t *)in);
+		in += sizeof(uint32_t);
+		flags = *(const uint8_t *)in;
+		in += sizeof(uint8_t);
+		block_desc = *(const uint8_t *)in;
+		in += sizeof(uint8_t);
 
-		in += sizeof(*h);
-		if (h->has_content_size)
+		version = (flags >> 6) & 0x3;
+		independent_blocks = (flags >> 5) & 0x1;
+		has_block_checksum = (flags >> 4) & 1;
+		has_content_size = (flags >> 3) & 0x1;
+
+		/* We assume there's always only a single, standard frame. */
+		if (magic != LZ4F_MAGICNUMBER || version != 1)
+			return 0;	/* unknown format */
+		if ((flags & 0x03) || (block_desc & 0x8f))
+			return 0;	/* reserved bits must be zero */
+		if (!independent_blocks)
+			return 0;	/* we don't support block dependency */
+
+		if (has_content_size) {
+			if (srcn < sizeof(uint32_t) + 3*sizeof(uint8_t) + sizeof(uint64_t))
+				return 0; /* input overrun */
 			in += sizeof(uint64_t);
+		}
+		/* Header checksum byte */
 		in += sizeof(uint8_t);
 	}
 
 	while (1) {
-		if ((size_t)(in - src) + sizeof(struct lz4_block_header) > srcn)
+		uint32_t block_header, block_size;
+
+		block_header = le32toh(*(const uint32_t *)in);
+		in += sizeof(uint32_t);
+		block_size = block_header & ~LZ4F_BLOCKUNCOMPRESSED_FLAG;
+
+		if ((size_t)(in - src) + block_size > srcn)
 			break;          /* input overrun */
 
-		struct lz4_block_header b = {
-			{ .raw = le32toh(*(const uint32_t *)in) }
-		};
-		in += sizeof(struct lz4_block_header);
-
-		if ((size_t)(in - src) + b.size > srcn)
+		if ((size_t)(in - src) + block_size > srcn)
 			break;			/* input overrun */
 
-		if (!b.size) {
+		if (!block_size) {
 			out_size = out - dst;
 			break;			/* decompression successful */
 		}
 
-		if (b.not_compressed) {
-			size_t size = MIN((uintptr_t)b.size, (uintptr_t)dst
+		if (block_header & LZ4F_BLOCKUNCOMPRESSED_FLAG) {
+			size_t size = MIN((uintptr_t)block_size, (uintptr_t)dst
 				+ dstn - (uintptr_t)out);
 			memcpy(out, in, size);
-			if (size < b.size)
+			if (size < block_size)
 				break;		/* output overrun */
 			out += size;
 		} else {
 			/* constant folding essential, do not touch params! */
-			int ret = LZ4_decompress_generic(in, out, b.size,
+			int ret = LZ4_decompress_generic(in, out, block_size,
 					dst + dstn - out, endOnInputSize,
 					full, 0, noDict, out, NULL, 0);
 			if (ret < 0)
@@ -162,7 +143,7 @@ size_t ulz4fn(const void *src, size_t srcn, void *dst, size_t dstn)
 			out += ret;
 		}
 
-		in += b.size;
+		in += block_size;
 		if (has_block_checksum)
 			in += sizeof(uint32_t);
 	}
