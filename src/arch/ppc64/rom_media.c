f2e74685378f342437ccbe5123cb17b939ef3252
Marty E. Plummer <hanetzer@startmail.com>
2020-10-06 07:26:51 -0500

    power9: hopefully fix boot
    Change-Id: Iede1b8311eaa031b9796c401526f16aab7b324c4
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/arch/ppc64/rom_media.c
+++ src/arch/ppc64/rom_media.c
@@ -1,11 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <boot_device.h>
+#include <stdint.h>
 
 /* This assumes that the CBFS resides at 0x0, which is true for the default
  * configuration. */
+#define rom_base ((void *) (uintptr_t)(0x6030000000000ULL-CONFIG_ROM_SIZE))
 static const struct mem_region_device boot_dev =
-	MEM_REGION_DEV_RO_INIT(NULL, CONFIG_ROM_SIZE);
+	MEM_REGION_DEV_RO_INIT(rom_base, CONFIG_ROM_SIZE);
 
 const struct region_device *boot_device_ro(void)
 {
