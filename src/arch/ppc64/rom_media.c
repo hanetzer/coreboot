3f87ea30d192c925a7340a5b6a5835df2607adaa
Marty E. Plummer <hanetzer@startmail.com>
2020-10-06 19:56:34 -0500

    ppc64: dummy down bootblock
    Change-Id: If3a9bc615ca8c543e9dcbc155decf04d6638df5a
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/arch/ppc64/rom_media.c
+++ src/arch/ppc64/rom_media.c
@@ -3,9 +3,14 @@
 #include <boot_device.h>
 #include <stdint.h>
 
+/*
+ * 0x60300f4000000
+ * 0x60300fc000000
+ */
 /* This assumes that the CBFS resides at 0x0, which is true for the default
  * configuration. */
-#define rom_base ((void *) (uintptr_t)(0x6030000000000ULL-CONFIG_ROM_SIZE))
+/* #define rom_base ((void *) (uintptr_t)(0x6030000000000ULL-CONFIG_ROM_SIZE)) */
+#define rom_base ((void *) (uintptr_t)(0x60300fc000000ULL))
 static const struct mem_region_device boot_dev =
 	MEM_REGION_DEV_RO_INIT(rom_base, CONFIG_ROM_SIZE);
 
