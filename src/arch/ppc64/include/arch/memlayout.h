00992aab24ceb67173add9c48498a90a494c8969
Marty E. Plummer <hanetzer@startmail.com>
2020-10-13 00:00:34 -0500

    soc: ibm: initial directory tree.
    The power9 directory is code which would be common to nimbus,
    cumulus, and axone processors.
    
    Change-Id: Ib50a0af3c59330bbc71682e9e17de81eee0b41e0
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/arch/ppc64/include/arch/memlayout.h
+++ src/arch/ppc64/include/arch/memlayout.h
@@ -8,6 +8,30 @@
 /* TODO: Double-check that that's the correct alignment for our ABI. */
 #define STACK(addr, size) REGION(stack, addr, size, 8)
 
+#define TOC(name, addr, size) \
+	SYMBOL(name, addr) \
+	.got : \
+	{ \
+		_toc_start = . + 0x8000; \
+		*(.got) \
+		*(.toc) \
+	} \
+	SYMBOL(e##name, addr + size)
+
+#if ENV_ROMSTAGE_OR_BEFORE
+	#define CAR_TOC(addr, size) \
+		TOC(car_toc, addr, size) \
+		ALIAS_REGION(car_toc, toc)
+	#define MEM_TOC(addr, size) \
+		REGION(mem_toc, addr, size, 4K)
+#else
+	#define CAR_TOC(addr, size) \
+		REGION(car_toc, addr, size, 4K)
+	#define MEM_TOC(addr, size) \
+		TOC(mem_toc, addr, size) \
+		ALIAS_REGION(mem_toc, toc)
+#endif
+
 /* TODO: Need to add DMA_COHERENT region like on ARM? */
 
 #endif /* __ARCH_MEMLAYOUT_H */
